eb28371ccb50e554cfab01c697045f8dd419e6d0
laercio <laercioigorps@gmail.com>
2021-04-27 06:07:45 -0300

    updating everything

--- fino/views.py
+++ fino/views.py
@@ -3,9 +3,10 @@ from django.db.models import Sum
 from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseForbidden
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.urls import reverse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 import datetime
+from django.db.models import Q
 
 from .forms import AccountModelForm, CattegoryModelForm, TransactionModelForm, Account_delete_form,TransactionTypeModelForm
 from .models import Account, Cattegory, Transaction
@@ -13,7 +14,16 @@ from .models import Account, Cattegory, Transaction
 # Create your views here.
 
 #views for account -----------------------------------------------------------------
+@login_required
+def wizard_view(request):
+    return HttpResponseRedirect(reverse('fino:home_page'))
 
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('fino:login_page'))
+    # Redirect to a success page.
 
 def teste_view(request):
 	return render(request,'fino/test.html')
@@ -36,6 +46,10 @@ def signup_view(request):
 
 @login_required
 def home_view(request):
+
+	current_month = datetime.date.today().month
+	current_year = datetime.date.today().year
+	
 	
 	accounts = request.user.account_set.all()
 
@@ -49,19 +63,22 @@ def home_view(request):
 	despesas_pendentes_transactions = incompleted_transactions.filter(cattegory__is_receita = False)
 	receitas_pendentes_transactions = incompleted_transactions.filter(cattegory__is_receita = True)
 
-	categorias_despesas = request.user.cattegory_set.filter(is_receita = False).filter(transaction__is_completed = True)
+	categorias_despesas = request.user.cattegory_set.filter(is_receita = False).filter(
+		transaction__is_completed = True)
 
 
-	transactions_despesas_all =transactions.filter(cattegory__is_receita = False)
-
-	despesas_por_categoria = categorias_despesas.values('name').annotate(total = Sum('transaction__total'))
 	
+
+	despesas_cat = Transaction.objects.filter(cattegory__user = request.user).filter(
+		cattegory__is_receita = False).filter(
+		date__month = current_month).filter(date__year = current_year).values('cattegory__name').annotate(
+		totals = Sum('total'))
 
 	labels_cat = []
 	data_cat = []
-	for cat in despesas_por_categoria:
-		labels_cat.append(cat['name'])
-		data_cat.append(str(cat['total']))
+	for cat in despesas_cat:
+		labels_cat.append(cat['cattegory__name'])
+		data_cat.append(str(cat['totals']))
 
 	despesas_pendentes_historico = despesas_pendentes_transactions.values('date__month').annotate(totals=Sum('total'))
 	receitas_pendentes_historico = receitas_pendentes_transactions.values('date__month').annotate(totals=Sum('total'))
@@ -91,7 +108,6 @@ def home_view(request):
 		receitas_pendentes_data[str(receita['date__month'])] = str(receita['totals'])
 
 	
-	current_month = datetime.date.today().month
 	
 	
 	context = {
@@ -140,18 +156,48 @@ def list_account_view(request):
 	#accounts = get_list_or_404(Account.objects.filter(user = request.user))
 	accounts = Account.objects.filter(user= request.user)
 
+	transactions = Transaction.objects.filter(account__user = request.user)
+
+
+	todas_mes = transactions.filter(date__month = 4)
+
+	pendentes = todas_mes.filter(is_completed = False)
+
+
+
+	#receitas = transactions.filter(is_completed = True).filter(cattegory__is_receita = True).values('total')
+
+	#receitas_pendentes = transactions.filter(is_completed = False).filter(cattegory__is_receita = True).values('total')
+
+	#despesas = transactions.filter(is_completed = True).filter(cattegory__is_receita = False).values('total')
+	#despesas_pendentes = transactions.filter(is_completed = False).filter(cattegory__is_receita = False).values('total')
+
+	#x = accounts.annotate(receitas = Sum(receitas)).annotate(despesas = Sum(despesas)).annotate(receitas_pendentes = Sum(receitas_pendentes)).annotate(despesas_pendentes = Sum(despesas_pendentes))
+
+	#accountss = accounts.filter(transaction__cattegory__is_receita = True).annotate(
+	#	receita_total = Sum('transaction__total'))
+
+	#accountss = accountss.filter(transaction__is_completed = True).annotate(receita_efetivada = Sum('transaction__total'))
+	
+	#accountss = accounts.filter(transaction__date__month = 4).values('name')
+
 	cat_labels = []
 	cat_data = []
+	
+	saldo = accounts.aggregate(Sum('total'))['total__sum']
 
+	valor_pendente = pendentes.aggregate(Sum('total'))['total__sum']
+	if not valor_pendente:
+		valor_pendente = 0
 	for account in accounts:
 		cat_labels.append(account.name)
 		cat_data.append(str(account.total))
-	print(cat_data)
-	context = {
+		context = {
 		'list_objects' : accounts,
 		'cat_labels' : cat_labels,
 		'cat_data': cat_data,
-		'saldo' : accounts.aggregate(Sum('total'))['total__sum']
+		'saldo' : saldo,
+		'previsto' : saldo + valor_pendente
 	}
 	return render(request,'fino/account_list.html', context)
 
@@ -272,6 +318,90 @@ def list_cattegory_view(request):
 
 
 @login_required
+def list_cattegory_by_month_year_view(request,month,year):
+
+	
+	#accounts = get_list_or_404(Account.objects.filter(user = request.user))
+	cattegory = Cattegory.objects.filter(user= request.user).filter(
+		transaction__date__year = year).filter(transaction__date__month = month)
+
+	receitas = Transaction.objects.filter(cattegory__user = request.user).filter(
+		cattegory__is_receita = True).filter(
+		date__month = month).filter(date__year = year).values('cattegory__name').annotate(
+		totals = Sum('total'))
+	despesas = Transaction.objects.filter(cattegory__user = request.user).filter(
+		cattegory__is_receita = False).filter(
+		date__month = month).filter(date__year = year).values('cattegory__name').annotate(
+		totals = Sum('total'))
+	
+
+	
+	cat_receitas_labels = []
+	cat_receitas_data = []
+
+	cat_despesas_labels = []
+	cat_despesas_data = []
+
+	#cat_receitas = cattegory.filter(is_receita = True).values('name').annotate(total = Sum('transaction__total'))
+	#cat_despesas = cattegory.filter(is_receita = False).values('name').annotate(total = Sum('transaction__total'))
+
+	
+	#receitas = cat_receitas.aggregate(Sum('total'))['total__sum']
+	#despesas= cat_despesas.aggregate(Sum('total'))['total__sum'] * -1
+	for cat in receitas:
+		if not  cat['totals']:
+			continue
+		cat_receitas_labels.append(cat['cattegory__name'])
+		cat_receitas_data.append(str(cat['totals']))
+
+	for cat in despesas:
+		if not  cat['totals']:
+			continue
+		cat_despesas_labels.append(cat['cattegory__name'])
+		cat_despesas_data.append(str(cat['totals']))
+
+	
+	cattegories = Cattegory.objects.filter(user = request.user).filter(
+		transaction__date__year = year).filter(transaction__date__month = month).distinct().annotate(
+		totals = Sum('transaction__total'))
+	
+
+	receitas_data  =receitas.aggregate(Sum('totals'))['totals__sum']
+	despesas_data = despesas.aggregate(Sum('totals'))['totals__sum']
+
+	if despesas_data:
+		despesas_data = despesas_data * -1
+
+	context = {
+		'list_objects' : cattegories,
+		'cat_receitas_labels' : cat_receitas_labels,
+		'cat_receitas_data' : cat_receitas_data,
+
+		'cat_despesas_labels' : cat_despesas_labels,
+		'cat_despesas_data' : cat_despesas_data,
+
+		'receitas' : receitas_data,
+		'despesas' : despesas_data,
+
+		'receitas_cat' : receitas,
+		'despesas_cat' : despesas,
+	}
+	return render(request,'fino/cattegory_list_by_month_year.html', context)
+
+
+@login_required
+def categorias_view(request):
+	#accounts = get_list_or_404(Account.objects.filter(user = request.user))
+	current_month = datetime.date.today().month
+	current_year = datetime.date.today().year
+	return HttpResponseRedirect(reverse('fino:cattegory_list_by_month_year',args = {current_month, current_year}))
+	transaction = Transaction.objects.filter(cattegory__user = request.user).order_by('-date')
+	context = {
+		'list_objects' : transaction
+	}
+	return render(request,'fino/transaction_list.html', context)
+
+@login_required
 def detail_cattegory_view(request, id):
 
 	cattegory = get_object_or_404(Cattegory,id = id)
@@ -379,11 +509,106 @@ def create_transaction_by_type_view(request, types):
 @login_required
 def list_transaction_view(request):
 	#accounts = get_list_or_404(Account.objects.filter(user = request.user))
-	transaction = Transaction.objects.filter(cattegory__user = request.user)
+	transaction = Transaction.objects.filter(cattegory__user = request.user).order_by('-date')
 	context = {
 		'list_objects' : transaction
 	}
 	return render(request,'fino/transaction_list.html', context)
+
+@login_required
+def transacoes_view(request):
+	#accounts = get_list_or_404(Account.objects.filter(user = request.user))
+	current_month = datetime.date.today().month
+	current_year = datetime.date.today().year
+	return HttpResponseRedirect(reverse('fino:transaction_list_by_month_year',args = {current_month, current_year}))
+	transaction = Transaction.objects.filter(cattegory__user = request.user).order_by('-date')
+	context = {
+		'list_objects' : transaction
+	}
+	return render(request,'fino/transaction_list.html', context)
+
+
+
+@login_required
+def list_transaction_view_by_month(request, month, year):
+	#accounts = get_list_or_404(Account.objects.filter(user = request.user))
+
+	months = {}
+	months['1'] = 'Janeiro'
+	months['2'] = 'Fevereiro'
+	months['3'] = 'Março'
+	months['4'] = 'Abril'
+	months['5'] = 'Maio'
+	months['6'] = 'Junho'
+	months['7'] = 'Julho'
+	months['8'] = 'Agosto'
+	months['9'] = 'Setembro'
+	months['10'] = 'Outubro'
+	months['11'] = 'Novembro'
+	months['12'] = 'Dezembro'
+
+	next_month = 0
+	before_month = 0
+	year_to_next = year
+	year_to_before = year
+
+	if month < 1 or month > 12:
+		raise Http404('mês invalido')
+
+	if month == 1:
+		before_month = 12
+		year_to_before = year - 1
+	else:
+		before_month = month -1
+
+	if month == 12:
+		next_month = 1
+		year_to_next = year +1
+	else:
+		next_month = month + 1
+
+
+
+	
+	transaction = Transaction.objects.filter(account__user = request.user).filter(date__year = year).filter(date__month  = month).order_by('-date')
+	
+	receitas = transaction.filter(cattegory__is_receita = True)
+	despesas = transaction.filter(cattegory__is_receita = False)
+
+
+	receitas_pendentes = receitas.filter(is_completed = False)
+	despesas_pendentes  = despesas.filter(is_completed = False)
+	
+	receitas_data = receitas.aggregate(total = Sum('total'))['total']
+	despesas_data = despesas.aggregate(total = Sum('total'))['total']
+	receitas_pendentes_data = receitas_pendentes.aggregate(total = Sum('total'))['total']
+	despesas_pendentes_data = despesas_pendentes.aggregate(total = Sum('total'))['total']
+
+	if(despesas_data):
+		despesas_data = despesas_data * -1
+	if(despesas_pendentes_data):
+		despesas_pendentes_data = despesas_pendentes_data * -1
+
+	context = {
+		'list_objects' : transaction,
+
+		'receitas' : receitas_data,
+		'despesas' : despesas_data,
+		'receitas_pendentes' : receitas_pendentes_data,
+		'despesas_pendentes' : despesas_pendentes_data,
+
+		'months' : months,
+		'year' : year,
+		'year_to_next' : year_to_next,
+		'year_to_before' : year_to_before,
+		'month' : months[str(month)],
+		'next_month' : next_month,
+		'next_month_name' : months[str(next_month)],
+		'before_month' : before_month,
+		'before_month_name' : months[str(before_month)],
+
+	}
+	return render(request,'fino/transaction_list_by_month.html', context)
 
 @login_required
 def list_transaction_by_account_view(request, cat):
