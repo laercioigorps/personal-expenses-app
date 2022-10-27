6c6fec59d709de3c8cf68c6004fc4b640965d7a0
Laercio Souza <laercioigorps@gmail.com>
2022-10-27 17:13:03 -0300

    created DateUtils and added MONTHS constant dictionary

--- fino/utils.py
+++ fino/utils.py
@@ -1,4 +1,23 @@
 from django.db.models import Sum
 from .models import Transaction
+
+
 def get_transactions_filter():
-	return Transaction.objects.values('date__month','date__year').annotate(soma=Sum('total'))
+    return Transaction.objects.values('date__month', 'date__year').annotate(soma=Sum('total'))
+
+
+class DateUtils:
+    MONTHS = {
+        '1': "Janeiro",
+        '2': "Fevereiro",
+        '3': "Março",
+        '4': "Abril",
+        '5': "Maio",
+        '6': "Junho",
+        '7': "Julho",
+        '8': "Agosto",
+        '9': "Setembro",
+        '10': "Outubro",
+        '11': "Novembro",
+        '12': "Dezembro",
+    }
